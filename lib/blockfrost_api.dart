bcd3b942c7c8a24d2667d69529ea3f1ebb5c5fc1
andro-devs <androdeveloptment@gmail.com>
2025-10-08 13:33:53 +0200

    feature: SignatureValidation
    - Export the src/utils/blockfrost_signature_validator.dart

--- lib/blockfrost_api.dart
+++ lib/blockfrost_api.dart
@@ -15,5 +15,6 @@ export 'src/scripts_service.dart';
 export 'src/transactions_service.dart';
 export 'src/utilities_service.dart';
 export 'src/ipfs_service.dart';
+export 'src/utils/blockfrost_signature_validator.dart';
 
 
